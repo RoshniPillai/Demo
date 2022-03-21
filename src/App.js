5e531089be6f7f9d6f8e14ca160cb9f577e9ff61
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 01:04:04 +0530

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ThemeConfig from "./theme";
 import GlobalStyles from "./theme/globalStyles";
 // components
 import ScrollToTop from "./components/ScrollToTop";
-import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
 
 // ----------------------------------------------------------------------
 
@@ -13,8 +12,7 @@ export default function App() {
   return (
     <ThemeConfig>
       <ScrollToTop />
-      <GlobalStyles />
-      <BaseOptionChartStyle />
+      <GlobalStyles />      
       <Router />
     </ThemeConfig>
   );
