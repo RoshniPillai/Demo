6f06b47f64faa1d248dc04ab7714e704a7dfca6f
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 00:49:41 +0530

    changes

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,18 +1,15 @@
-import { motion } from 'framer-motion';
-import { Link as RouterLink } from 'react-router-dom';
-// material
-import { styled } from '@mui/material/styles';
-import { Box, Button, Typography, Container } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Button, Typography, Container } from "@mui/material";
 // components
-import { MotionContainer, varBounceIn } from '../components/animate';
-import Page from '../components/Page';
-
+import { MotionContainer, varBounceIn } from "../components/animate";
+import Page from "../components/Page";
+import { Link as RouterLink } from "react-router-dom";
 // ----------------------------------------------------------------------
 
 const RootStyle = styled(Page)(({ theme }) => ({
-  display: 'flex',
-  minHeight: '100%',
-  alignItems: 'center',
+  display: "flex",
+  minHeight: "100%",
+  alignItems: "center",
   paddingTop: theme.spacing(15),
   paddingBottom: theme.spacing(10)
 }));
@@ -21,33 +18,33 @@ const RootStyle = styled(Page)(({ theme }) => ({
 
 export default function Page404() {
   return (
-    <RootStyle title="404 Page Not Found | Minimal-UI">
+    <RootStyle title="404 Page Not Found">
       <Container>
-        <MotionContainer initial="initial" open>
-          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
-            <motion.div variants={varBounceIn}>
-              <Typography variant="h3" paragraph>
-                Sorry, page not found!
-              </Typography>
-            </motion.div>
-            <Typography sx={{ color: 'text.secondary' }}>
-              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL?
-              Be sure to check your spelling.
-            </Typography>
+        <Box sx={{ maxWidth: 480, margin: "auto", textAlign: "center" }}>
+          <Typography variant="h3" paragraph>
+            Sorry, page not found!
+          </Typography>
 
-            <motion.div variants={varBounceIn}>
-              <Box
-                component="img"
-                src="/static/illustrations/illustration_404.svg"
-                sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
-              />
-            </motion.div>
+          <Typography sx={{ color: "text.secondary" }}>
+            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
+            mistyped the URL? Be sure to check your spelling.
+          </Typography>
 
-            <Button to="/" size="large" variant="contained" component={RouterLink}>
-              Go to Home
-            </Button>
-          </Box>
-        </MotionContainer>
+          <Box
+            component="img"
+            src="/static/illustrations/illustration_404.svg"
+            sx={{ height: 260, mx: "auto", my: { xs: 5, sm: 10 } }}
+          />
+
+          <Button
+            to="/"
+            size="large"
+            variant="contained"
+            component={RouterLink}
+          >
+            Go to Home
+          </Button>
+        </Box>
       </Container>
     </RootStyle>
   );
