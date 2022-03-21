3c751c5f55dc16a61a52eff4fac4e42f89c3398b
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-21 23:30:11 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+// // routes
+import Router from "./routes";
+// theme
+import ThemeConfig from "./theme";
+import GlobalStyles from "./theme/globalStyles";
+// components
+import ScrollToTop from "./components/ScrollToTop";
+import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
+
+// ----------------------------------------------------------------------
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
   );
 }
