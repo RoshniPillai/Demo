29b3beb73bc36028d8aa3523f2a12fb317126074
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 01:27:47 +0530

    change

---
+++ src/sections/@dashboard/app/AppCurrentVisits.js
@@ -0,0 +1,18 @@
+import { merge } from 'lodash';
+
+import { useTheme, styled } from '@mui/material/styles';
+import { Card, CardHeader } from '@mui/material';
+
+
+export default function AppCurrentVisits() {
+  const theme = useTheme();
+
+
+  return (
+    <Card>
+      <CardHeader title="Current Visits" />
+      <ChartWrapperStyle dir="ltr">
+        </ChartWrapperStyle>
+    </Card>
+  );
+}
