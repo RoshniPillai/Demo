da351abfcaeef0c28b2a4fc0bf1bd5226026733d
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 02:30:15 +0530

    Add files via upload

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -1,37 +1,37 @@
-import PropTypes from "prop-types";
-//import SimpleBarReact from 'simplebar-react';
+import PropTypes from 'prop-types';
+import SimpleBarReact from 'simplebar-react';
 // material
-import { alpha, styled } from "@mui/material/styles";
-import { Box } from "@mui/material";
+import { alpha, styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled("div")({
+const RootStyle = styled('div')({
   flexGrow: 1,
-  height: "100%",
-  overflow: "hidden"
+  height: '100%',
+  overflow: 'hidden'
 });
 
-// const SimpleBarStyle = styled(SimpleBarReact)(({ theme }) => ({
-//   maxHeight: '100%',
-//   '& .simplebar-scrollbar': {
-//     '&:before': {
-//       backgroundColor: alpha(theme.palette.grey[600], 0.48)
-//     },
-//     '&.simplebar-visible:before': {
-//       opacity: 1
-//     }
-//   },
-//   '& .simplebar-track.simplebar-vertical': {
-//     width: 10
-//   },
-//   '& .simplebar-track.simplebar-horizontal .simplebar-scrollbar': {
-//     height: 6
-//   },
-//   '& .simplebar-mask': {
-//     zIndex: 'inherit'
-//   }
-// }));
+const SimpleBarStyle = styled(SimpleBarReact)(({ theme }) => ({
+  maxHeight: '100%',
+  '& .simplebar-scrollbar': {
+    '&:before': {
+      backgroundColor: alpha(theme.palette.grey[600], 0.48)
+    },
+    '&.simplebar-visible:before': {
+      opacity: 1
+    }
+  },
+  '& .simplebar-track.simplebar-vertical': {
+    width: 10
+  },
+  '& .simplebar-track.simplebar-horizontal .simplebar-scrollbar': {
+    height: 6
+  },
+  '& .simplebar-mask': {
+    zIndex: 'inherit'
+  }
+}));
 
 // ----------------------------------------------------------------------
 
@@ -47,11 +47,17 @@ export default function Scrollbar({ children, sx, ...other }) {
 
   if (isMobile) {
     return (
-      <Box sx={{ overflowX: "auto", ...sx }} {...other}>
+      <Box sx={{ overflowX: 'auto', ...sx }} {...other}>
         {children}
       </Box>
     );
   }
 
-  return <RootStyle></RootStyle>;
+  return (
+    <RootStyle>
+      <SimpleBarStyle timeout={500} clickOnTrack={false} sx={sx} {...other}>
+        {children}
+      </SimpleBarStyle>
+    </RootStyle>
+  );
 }
