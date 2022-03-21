981bec0f024984c57f9398ac47993fb27d3ab879
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 02:28:59 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // scroll bar
-import "simplebar/src/simplebar.css";
+import 'simplebar/src/simplebar.css';
 
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 //
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import reportWebVitals from "./reportWebVitals";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 // ----------------------------------------------------------------------
 
@@ -18,13 +18,13 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </HelmetProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to enable client cache, register instead.
-//serviceWorker.unregister();
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
